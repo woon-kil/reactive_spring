1969f3f8719d4294e4243673a816fe8b0aee3223
woonkil <woonkil70@gmail.com>
2022-04-19 21:12:46 +0900

    0419 2112

--- src/main/java/org/example/customjavareactapp/service/CustomService.java
+++ src/main/java/org/example/customjavareactapp/service/CustomService.java
@@ -10,6 +10,10 @@ import java.time.Duration;
 @Service
 public class CustomService {
     public Mono<Data> getData(long threadId, long time) {
-        return Mono.just(Data.builder().data("Some Data").threadId(threadId).requestCameTime(time).build()).delayElement(Duration.ofSeconds(20));
+        //return Mono.just(Data.builder().data("Some Data").threadId(threadId).requestCameTime(time).build()).delayElement(Duration.ofSeconds(20));
+        return Mono.just(Data.builder().data("Some Data").threadId(threadId)
+                .requestCameTime(time).build())
+                .delayElement(Duration.ofSeconds(1));
+
     }
 }
